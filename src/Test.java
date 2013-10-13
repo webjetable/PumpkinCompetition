c064a3208e1eb996cc9601fe918f82d23ee61a29
webjetable <brigitte@withalm.net>
2013-10-13 12:54:14 +0200

    Update Test
    ...habe neuen testfall ergänzt: neue competition.
    wäre es nicht praktisch wenn man auf tages-array zugreifen könnte, weil
    es auf private ist und es keinen dazugehörigen getter gibt geht das
    momentan nicht...
    finde auch die benennung mit "days" für die Wettbewerbsdauer etwas
    verwirrend (duration zB wäre sprechender, od?). Dafür glaub ich wäre
    auch ein Getter sinnvoll, od?

--- src/Test.java
+++ src/Test.java
@@ -1,3 +1,4 @@
+
 public class Test {
 
 	/**
@@ -5,44 +6,127 @@ public class Test {
 	 */
 	public static void main(String[] args) {
 
-		
+
 		System.out.println("Test Neuer Kuerbis" + "\n");
-		testNeuerKuerbis();
-		
+		testPumpkin();
+
 		System.out.println("Test Gewichtszunahme unter normalen Bedingungen");
 		testStandardGrow();
+
+		System.out.println("Test 5-Tages Trockenphase");
+		testDrought5Days();
+
+		System.out.println("Test Neue Competition");
+		testCompetition();
 
 	}
 
 	// Test der Klasse Pumpkin
-	private static void testNeuerKuerbis(){
+	private static void testPumpkin(){
 		Pumpkin p = new Pumpkin();
 		System.out.println("Gewicht: " + p.getWeight()); // erwarteter Wert: 1
-		System.out.println("Alter: " + p.getAge()); // erwarteter Wert: 0
+		System.out.println("Gewicht: " + p.getAge()); // erwarteter Wert: 0
 		System.out.println();
 	}
 
 	/*
-		Test Gewichtszunahme unter normalen Bedingungen (5Prozent Wachstum bei 100Prozent Licht,
-		sonst anteilsmaessig geringer)		
+	 * Test Gewichtszunahme unter normalen Bedingungen (5Prozent Wachstum bei 100Prozent Licht,
+	 * sonst anteilsmaessig geringer)			
 	 */
+
 	private static void testStandardGrow(){
-		
+
 		Pumpkin p = new Pumpkin();
 
 		Day firstDay = new Day(20); // 1.Tag: 80 Prozent Sonne -> 4 Prozent Wachstum
 		p.grow(firstDay);
 		System.out.println("Gewicht Tag 1: " + p.getWeight()); //erwarteter Wert: 1,04
-		
+
 		Day secondDay = new Day(30); // 2.Tag: 70 Prozent Sonne -> 3,5 Prozent Wachstum
 		p.grow(secondDay);
 		System.out.println("Gewicht Tag 2: " + p.getWeight()); //erwarteter Wert: 1,0764
+		System.out.println();
 	}
+
+	/*
+	 * Test Wachstumseinschraenkung durch 5-taegige Trockenperiode
+	 * In Trockenperioden stellt der Kuerbis das Wachstum ein. Hat es in den letzten 5 Tagen 
+	 * zusammengenommen weniger als 10 Prozent Wasser gegeben, geht das Wachstum auf die 
+	 * Haelfte zurueck. 
+	 */
+
+	private static void testDrought5Days(){
+
+		Pumpkin p = new Pumpkin();
+		Days[] d = new Days[12];
+
+		// 1. bis 5.Tag: je 80 Prozent Sonne -> 4 Prozent Wachstum
+		for (int i =0; i<5; i++){
+			d.add(new Day(20));
+		}
+
+		// 6. bis 10.Tag: je 99 Prozent Sonne (nur 1 Prozent Wasser) -> 4,95 Prozent Wachstum
+		for (int i =0; i<5; i++){
+			d.add(new Day(1));
+		}
+		// 11. bis 12.Tag: je 20 Prozent Wasser -> nur 2 statt 4 Prozent Wachstum wg. Trockenphase
+		for (int i =0; i<2; i++){
+			d.add(new Day(20));
+		}
+
+
+		for(int i=0; i<d.length; i++){
+			p.grow(d[i]);
+		}
+
+		System.out.println("Gewicht nach 2 Tage nach 5 taegiger Trockenphase: " 
+				+ p.getWeight()); //erwarteter Wert: 1,62
+		System.out.println();
+
+	}
+
+
+	/*
+	 * Test der Klasse Competition
+	 * 
+	 */
+
+	private static void testCompetition(){
+
+		Competition c = new Competition(2);
+
+		Day firstDay = new Day(20);
+		Day secondDay = new Day(80);
+
+		try{
+			c.setNextDay(firstDay);
+		}
+		catch (CompException ce){
+			System.out.println(ce.getMessage());
+		}
+		
+		try{
+			c.setNextDay(secondDay);
+		}
+		catch (CompException ce){
+			System.out.println(ce.getMessage());
+		}
+		
+		System.out.println("Wettbewerbsdauer: " + c.getDuration()); // erwarteter Wert: 2
+		System.out.println();
+		
+	}
+
+
+
 
 }
 
 /*
  * 
+ * Jonas      , Matrnr           : Klasse Pumpkin
+ * Angelika Fuerndraht-Grossschopf, Matrnr       : Klassen Competition und Day
+ * Brigitte Withalm, Matrnr 1126733: Testklasse
  * 
  * gemeinsam: Interpretation der Aufgabenstellung und Konzept
  * 
@@ -51,17 +135,16 @@ public class Test {
 /*
 weiters zu testen
 
-In Trockenperioden stellt der Kï¿½rbis das Wachstum ein. Hat es in den letzten 5 Tagen 
-zusammengenommen weniger als 10% Wasser gegeben, geht das Wachstum auf die Hï¿½lfte zurï¿½ck. 
-Hat es in den letzten 10 Tagen zusammengenommen weniger als 10% Wasser gegeben, wird das 
-Wachstum sogar ganz eingestellt.
+ * trockenheit 2.fall
+ * Hat es in den letzten 10 Tagen zusammengenommen weniger als 10% Wasser gegeben, wird das 
+ * Wachstum sogar ganz eingestellt.
 
-Schnecken lieben Feuchtigkeit, vor allem gleichmï¿½ï¿½ige Feuchtigkeit. An jedem Tag mit 
-mindestens 50% Wasser machen sie sich ï¿½ber die Blï¿½tter her und sorgen dafï¿½r, dass der Kï¿½rbis 
-1% seines Gewichts verliert. Ab 30% Wasser in den letzten 2 Tagen verliert der Kï¿½rbis 
+Schnecken lieben Feuchtigkeit, vor allem gleichmäßige Feuchtigkeit. An jedem Tag mit 
+mindestens 50% Wasser machen sie sich über die Blätter her und sorgen dafür, dass der Kürbis 
+1% seines Gewichts verliert. Ab 30% Wasser in den letzten 2 Tagen verliert der Kürbis 
 ebenso 1% des Gewichts, genauso wie ab 10% Wasser in den letzten 4 Tagen.
 
-Die tatsï¿½chlichen klimatischen Bedingungen dï¿½rfen nicht ganz auï¿½er Acht gelassen werden. 
+Die tatsächlichen klimatischen Bedingungen dürfen nicht ganz außer Acht gelassen werden. 
 Daher muss es mindestens 10 Tage mit 100% Wasser und mindestens 10 Tage mit 100% Sonne geben.
-Wird der Kï¿½rbis an einem Tag gar nicht versorgt, erhï¿½lt er weder Wasser noch Licht.
+Wird der Kürbis an einem Tag gar nicht versorgt, erhält er weder Wasser noch Licht.
  */
