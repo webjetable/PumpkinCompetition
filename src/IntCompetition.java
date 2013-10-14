c6ff0aa6649868adad0bac1c997c6714dab1276b
bisam <rattifant@gmx.at>
2013-10-15 00:59:17 +0200

    Exception Würfe angepasst

--- src/IntCompetition.java
+++ src/IntCompetition.java
@@ -1,7 +1,6 @@
-
 public interface IntCompetition {
 
-	public void setNextDay(Day day) throws CompException;
-	public double getAvgSun( int days);
-	public double getAvgRain( int days);
+	public void setNextDay( Day day ) throws CompException;
+	public double getAvgSun( int days ) throws CompException;
+	public double getAvgRain( int days ) throws CompException;
 }
